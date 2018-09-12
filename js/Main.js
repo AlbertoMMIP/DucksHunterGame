a4cc47d399ab56eca891de1b7a5c76104bbe9bfe
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-09-11 19:44:45 -0500

    Se modifica tamaño del score

--- js/Main.js
+++ js/Main.js
@@ -9,7 +9,7 @@ var playerA = new Element(400,300,50,50,ctx,"./images/arma.png");
 var playerB = new Element(800,300,50,50,ctx,"./images/armaB.png");
 var gunA = new Weapon(playerA.x,playerA.y,50,30,ctx,"./images/duckB_8.png");
 var gunB = new Weapon(playerB.x,playerB.y,50,30,ctx,"./images/duckA_8.png");
-var duckScoreA = new Ducks(30,25,70,50,ctx,"./images/duckA_0.png","a");
-var duckScoreB = new Ducks(canvas.width-90,25,70,50,ctx,"./images/duckB_4.png","b");
+var duckScoreA = new Ducks(40,25,50,30,ctx,"./images/duckA_0.png","a");
+var duckScoreB = new Ducks(canvas.width-80,25,50,30,ctx,"./images/duckB_4.png","b");
 start();
 
